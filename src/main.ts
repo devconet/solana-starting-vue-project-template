559f5924283de03688a114ebeb7b7710d5b1e714
Devconet <42451250+devconet@users.noreply.github.com>
2025-03-03 16:20:59 +0100

    Added solana-wallet packages and then added connect button to the template

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import SolanaWallets from "solana-wallets-vue";
 
-createApp(App).mount('#app')
+// You can either import the default styles or create your own.
+import "solana-wallets-vue/styles.css";
+
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+const walletOptions = {
+    wallets: [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+    ],
+    autoConnect: true,
+  };
+
+  const app = createApp(App);
+  app.use(SolanaWallets, walletOptions);
+  app.mount('#app');
